26ed69981d6c2c919444e0209efb90bebc67dc7a
quindel <qjordan16@yahoo.com>
2024-09-11 23:29:31 -0500

    first commit

---
+++ src/test/java/com/springDi/springPratice/SpringPraticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springDi.springPratice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPraticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
